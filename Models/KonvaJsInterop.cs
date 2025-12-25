c1f24a7ddb892e280a1b6bcf6dc06df6e462682c
EECSB <contact@eecs.blog>
2025-12-26 00:40:50 +0100

    /

---
+++ Models/KonvaJsInterop.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using static System.Net.WebRequestMethods;
+
+namespace GDSViewer.JsInterop
+{
+    public class KonvaJsInterop
+    {
+        //Constructors ///////////////////////////////////////////////
+
+        public KonvaJsInterop(IJSRuntime js)
+        {
+            JS = js;
+        }
+
+        //////////////////////////////////////////////////////////////
+        
+
+        //Properties /////////////////////////////////////////////////
+
+        public IJSRuntime JS { get; set; }
+
+        //////////////////////////////////////////////////////////////
+
+
+        //Methods ////////////////////////////////////////////////////
+
+        public ValueTask InitializeKonva() 
+        {
+            return JS.InvokeVoidAsync("initKonva", new {});
+        }
+  
+        //////////////////////////////////////////////////////////////
+    }
+}
